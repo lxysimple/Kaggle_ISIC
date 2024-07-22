ba75cbcc00f7ed9ce98c6eb8a3d9b2302deafab6
lxysimple <128936873+lxysimple@users.noreply.github.com>
2024-07-22 22:02:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -630,10 +630,14 @@ def prepare_loaders(df, fold):
     valid_dataset = ISICDataset(df_valid, HDF_FILE, transforms=data_transforms["valid"])
 
     train_dataset2020 = ISICDataset_for_Train_fromjpg('/home/xyli/kaggle/data2020', transforms=data_transforms["train"])
+    train_dataset2019 = ISICDataset_for_Train_fromjpg('/home/xyli/kaggle/data2019', transforms=data_transforms["train"])
+    train_dataset2018 = ISICDataset_for_Train_fromjpg('/home/xyli/kaggle/data2018', transforms=data_transforms["train"])
 
-    concat_dataset = ConcatDataset([train_dataset, train_dataset2020])
+    concat_dataset = ConcatDataset([
+        train_dataset, train_dataset2020, train_dataset2019, train_dataset2018
+    ])
 
-    train_loader = DataLoader(train_dataset, batch_size=CONFIG['train_batch_size'], 
+    train_loader = DataLoader(concat_dataset, batch_size=CONFIG['train_batch_size'], 
                               num_workers=16, shuffle=True, pin_memory=True, drop_last=True)
     valid_loader = DataLoader(valid_dataset, batch_size=CONFIG['valid_batch_size'], 
                               num_workers=16, shuffle=False, pin_memory=True)
