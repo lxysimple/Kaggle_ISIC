e9ce5957bcd0911bf392e28a3ca367226a404cb2
lxysimple <128936873+lxysimple@users.noreply.github.com>
2024-08-25 10:12:52 +0800

    eva+2020（1:10）

--- train4.py
+++ train4.py
@@ -650,7 +650,7 @@ data_transforms = {
                 mean=[0.485, 0.456, 0.406], 
                 std=[0.229, 0.224, 0.225], 
                 max_pixel_value=255.0, 
-                p=1.0
+                p=1.0 
             ),
         ToTensorV2()
     ], p=1.),
